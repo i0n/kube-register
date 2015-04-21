0a284a6f07888f2b22dd6116e919121e87436a42
Kelsey Hightower <kelsey.hightower@gmail.com>
2015-04-21 06:00:29 +0200

    register: use v1beta3 API

--- register.go
+++ register.go
@@ -3,57 +3,73 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Minion struct {
-	Kind       string `json:"kind,omitempty"`
-	ID         string `json:"id,omitempty"`
-	HostIP     string `json:"hostIP,omitempty"`
-	APIVersion string `json:"apiVersion,omitempty"`
+type Node struct {
+	Kind       string   `json:"kind,omitempty"`
+	APIVersion string   `json:"apiVersion,omitempty"`
+	Metadata   Metadata `json:"metadata,omitempty"`
+	Spec       Spec     `json:"spec,omitempty"`
 }
 
-type MinionResp struct {
+type Metadata struct {
+	Name string `json:"name,omitempty"`
+}
+
+type Spec struct {
+	ExternalID string `json:"externalID,omitempty"`
+}
+
+type NodeResp struct {
 	Reason string `json:"reason,omitempty"`
 }
 
 func register(endpoint, addr string) error {
-	m := &Minion{
-		Kind:       "Minion",
-		APIVersion: "v1beta1",
-		ID:         addr,
-		HostIP:     addr,
-	}
-	mr := &MinionResp{}
-	data, err := json.Marshal(m)
+	var n Node
+	n.Kind = "Node"
+	n.APIVersion = "v1beta3"
+	n.Metadata.Name = addr
+	n.Spec.ExternalID = addr
+
+	data, err := json.Marshal(&n)
 	if err != nil {
 		return err
 	}
-	url := fmt.Sprintf("%s/api/v1beta1/minions", endpoint)
+
+	url := fmt.Sprintf("%s/api/v1beta3/nodes", endpoint)
+
 	res, err := http.Post(url, "application/json", bytes.NewReader(data))
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
-	if res.StatusCode == 202 || res.StatusCode == 200 {
+
+	if res.StatusCode == 202 || res.StatusCode == 200 || res.StatusCode == 201 {
 		log.Printf("registered machine: %s\n", addr)
 		return nil
 	}
+
 	data, err = ioutil.ReadAll(res.Body)
-	json.Unmarshal([]byte(data), &mr)
-	if res.StatusCode == 409 && mr.Reason == "AlreadyExists" {
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != 409 {
+		return fmt.Errorf("error registering: %s %d %s", addr, res.StatusCode, string(data))
+	}
+
+	nr := &NodeResp{}
+	if err := json.Unmarshal([]byte(data), &nr); err != nil {
+		return err
+	}
+
+	if res.StatusCode == 409 && nr.Reason == "AlreadyExists" {
 		return nil
 	}
-	log.Printf("Response: %#v", res)
-	log.Printf("Response Body:\n%s", string(data))
-	body, err := ioutil.ReadAll(res.Body)
-	reason := ""
-	if err == nil {
-		reason = ": " + string(body)
-	}
-	return errors.New("error registering: " + addr + reason)
+
+	return fmt.Errorf("error registering: %s %s", addr, nr.Reason)
 }
